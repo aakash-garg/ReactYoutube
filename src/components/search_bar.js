a524eea59fd11c5a683a307b0a539a888d75f086
Aakash Garg <aakash.garg80@gmail.com>
2017-09-03 01:11:18 +0530

    searchbar event_handler_clean_up

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,11 +3,7 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component{
   render() {
-    return <input onChange = {this.onInputChange}/>;
-  }
-
-  onInputChange(event){
-    console.log(event.target.value);
+    return <input onChange = {event => console.log(event.target.value)}/>;
   }
 }
 
