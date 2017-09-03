897273e6c6f1e5af1075dbde2b083f5b793096b3
Aakash Garg <aakash.garg80@gmail.com>
2017-09-03 10:49:56 +0530

    update video state and class based App

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBrb72kJJnJghmnvzqoTt4B0dwsH_2Gsk8';
 
-const App = () => {
-  return(
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+      this.setState({videos});
+      // this.setState({videos: videos});
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
