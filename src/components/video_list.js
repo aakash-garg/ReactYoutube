030facaa767e8f4b196788fc035ed53938303150
Aakash Garg <aakash.garg80@gmail.com>
2017-09-03 13:48:26 +0530

    add video_list_item component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item'
 
 const VideoList = (props) => {
   const videos = props.videos
   //array of videos.. props is what passed to child Component in index.js.. in class based component it is accessed by this.props
+  const videoItems = videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+  //videoItems is an array of videos
   return(
     <ul className="col-md-4 list-group">
-      {videos.length}
+      {videoItems}
     </ul>
   );
 }
