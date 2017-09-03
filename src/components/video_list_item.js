5c7be66e2fbed95535dc2358a58b3b2038e6bfb8
Aakash Garg <aakash.garg80@gmail.com>
2017-09-03 14:05:33 +0530

    arrange api content

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => { // (props) =>
+  // const video = props.video;
+  const imageURL = video.snippet.thumbnails.default.url;
+  console.log(video);
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageURL} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
