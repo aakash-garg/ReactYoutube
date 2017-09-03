f6c71ea5bb0dda162f3742359ff2d332acccb93f
Aakash Garg <aakash.garg80@gmail.com>
2017-09-03 16:41:56 +0530

    add video select and creating selectedVideo prop

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,7 +5,13 @@ const VideoList = (props) => {
   const videos = props.videos
   //array of videos.. props is what passed to child Component in index.js.. in class based component it is accessed by this.props
   const videoItems = videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
+    return(
+      <VideoListItem
+        onVideoSelect = {props.onVideoSelect}
+        key={video.etag}
+        video={video}
+      />
+    );
   });
   //videoItems is an array of videos
   return(
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => { // (props) =>
+const VideoListItem = ({video, onVideoSelect}) => { // (props) =>
   // const video = props.video;
   const imageURL = video.snippet.thumbnails.default.url;
-  console.log(video);
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageURL} />
