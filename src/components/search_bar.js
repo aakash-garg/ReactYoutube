ede5e4f3385b080a9e2a8fc213c3198c8fba4f3e
Aakash Garg <aakash.garg80@gmail.com>
2017-09-03 17:24:21 +0530

    add search bar functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,15 +8,22 @@ class SearchBar extends Component{
 
     this.state = {term: ''};
   }
+
   render() {
     return(
       <div className="search-bar">
         <input
           value = {this.state.term}
-          onChange = {event => this.setState({term: event.target.value })}/>
+          onChange = {event => this.onInputChange(event.target.value)}/>
       </div>
     );
   }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -19,19 +19,25 @@ class App extends Component{
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
-      this.setState({
+      // this.setState({videos: videos});
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term){
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+        this.setState({
         videos: videos,
         selectedVideo: videos[0]
+        });
       });
-      // this.setState({videos: videos});
-    });
   }
 
   render(){
     return(
       <div>
-        <SearchBar />
+        <SearchBar
+          onSearchTermChange={term => this.videoSearch(term)}
+        />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
